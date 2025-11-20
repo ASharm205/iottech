31a94d791f288831df760aae35fd0c2ef3f15656
ASharm205 <148150075+ASharm205@users.noreply.github.com>
2025-11-20 14:02:52 -0500

    naviagtion pages

--- iottech-consulting/src/pages/CaseStudiesPage.jsx
+++ iottech-consulting/src/pages/CaseStudiesPage.jsx
@@ -165,7 +165,7 @@ function CaseStudiesPage() {
       <div className="case-studies-header">
         <h2>Client Stories</h2>
         <div className="header-actions">
-          <button className="nav-link btn-primary" onClick={openAddForm}>Add Case Study</button>
+          <button className="nav-link btn-primary diagnostic-force" onClick={openAddForm}>Add Case Study</button>
         </div>
       </div>
 
